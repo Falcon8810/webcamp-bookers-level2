1107157d0b3838c0d94882691cb7c805674536a0
Falcon8810 <hayato.sa9211@gmail.com>
2021-10-17 14:19:14 +0000

    5-10課題3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '130d3ee8b30d78260e11b645bc98424584a8c88ab39db7c209fa7587c09cec80f79052aec1d91e25dae0b5040d97d68e878dd80892b8d30f2059104e45e440c8'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about" , as: "about"
   resources :books
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
